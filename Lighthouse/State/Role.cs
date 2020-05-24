a079252e1a20bc966935004f15a46a0016990830
Adam Hoka <adam.hoka@gmail.com>
2020-05-24 15:13:15 +0200

    Add state enum

---
+++ Lighthouse/State/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lighthouse.State
+{
+    public enum Role
+    {
+        Follower,
+        Candidate,
+        Leader
+    }
+}
